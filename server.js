14a23427ee323be3a85eb78f388fa55ecd905a13
Marc Boanas <marcjohnboanas@gmail.com>
2018-05-23 13:49:50 +0100

    Api route & server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+// Modules
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const cors = require('cors');
+
+// Config
+const config = require('./server/config');
+
+// Api
+const app = express();
+const port = process.env.PORT || 8083;
+app.set('port', port);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(cors());
+
+// Database configuration
+mongoose.connect(config.MONGO_URI);
+
+if process.env.NODE_ENV === 'dev' {
+	mongoose.set('debug', true);
+}
+
+// Pass routing to Angular app
+// Don't run in dev
+
+if (process.env.NODE_ENV !== 'dev') {
+	app.get('*', function(req, res) {
+		res.sendFile(path.join(__dirname, '/dist/index.html'));
+	});
+}
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(port, () => {
+	console.log(`server is listening on port ${port}`);
+});
---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	AUTH0_DOMAIN: 'mjboanas.eu.auth0.com',
+	AUTH0_API_AUDIENCE: 'http://localhost:8083/api/',
+	MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/production-app-01',
+	NAMESPACE: 'http://myapp.com/roles'
+};
